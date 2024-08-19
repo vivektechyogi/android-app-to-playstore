a1d898c49e1d6b8179d127743f63a790138d10ff
Vivek <iamviveklalan@gmail.com>
2024-08-19 16:31:35 +0530

    version 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.techyogi.apptoplaystore"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
